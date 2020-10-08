c70093a6329d39699fbc962c84808d778fd4f51b
irmalg <lopz.irma@gmail.com>
2020-10-08 09:13:50 -0500

    Archivo nuevo
    Añadimos archivo nuevo

--- introduction/area.c
+++ introduction/area.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include<stdio.h> //Input Output
 #include<math.h>
-#define PI 3.1416
+#define PI 3.1416 //Definiendo una constante
 
 int main(){
-	float area, radio;
-	radio = 5;
-	area = PI * pow(5,2);
-	printf("Area\n");
-	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
+	float area, radio; //Declarando variables de tipo flotante
+	radio = 5; //Asignando valor a variable radio
+	area = PI * pow(5,2); //Adignando resultado de la operación a variable radio
+	printf("Area\n"); //Imprimiendo título
+	// Imprimiento resultado
+	printf("Area de Circulo con radio 5: %f", area);
+	return 0;
 }
